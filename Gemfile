fa8befb44ceaaca21db2c810455cf355a31b0376
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-07 23:49:40 +0500

    Connect to react front end

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'sprockets-rails'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
 
+gem 'rack-cors'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
 
@@ -72,3 +74,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+gem "active_model_serializers", "~> 0.10.13"
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :greetings, only: [:index]
+      resources :greetings, only: :index, defaults: { format: :json }
     end
   end
   # Defines the root path route ("/")
